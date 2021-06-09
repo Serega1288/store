80794ef98796843c5aa07f5ec863153d709c3b29
Сергей <svtomusjak@gmail.com>
2021-06-09 13:25:23 +0300

    Update get_image_size.js

--- src/components/global/get_image_size.js
+++ src/components/global/get_image_size.js
@@ -4,13 +4,13 @@ function get_image_size(imgItem, size) {
     var index, src;
     for (index = 0; index < imgItem?.mediaDetails?.sizes.length; ++index) {
         if ( imgItem?.mediaDetails?.sizes[index].name === size) {
-            src = imgItem.mediaDetails.sizes[index].sourceUrl;
+            src = imgItem?.mediaDetails?.sizes[index].sourceUrl;
         }
     }
     if ( src ) {
         return src;
     } else {
-        return imgItem.mediaItemUrl;
+        return imgItem?.mediaItemUrl;
     }
 }
 
